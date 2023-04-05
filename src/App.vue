184c87083053cab427c4d2d93b4b6a57f5a96a25
tzubindev <71303907+tzubindev@users.noreply.github.com>
2023-04-05 10:19:22 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,8 @@
 						</div>
 						<div class="flex flex-col space-y-1 mt-4 -mx-2 h-full">
 							<button v-for="user in filteredUsers" :key="user.name" class="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-								<div class="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">{{ user.image }}</div>
-								<div class="ml-2 text-sm font-semibold">{{ user.name }}</div>
+								<div class="ml-2 flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">{{ user.image }}</div>
+								<div class="ml-2 truncate text-sm font-semibold">{{ user.name }}</div>
 							</button>
 						</div>
 					</div>
@@ -57,13 +57,13 @@
 						</div>
 						<div class="flex justify-end">
 							<div v-for="response in suggestedResponse" :key="response">
-								<button class="border border-gray-200 bg-white hover:bg-indigo-500 rounded-lg py-1 px-2 ml-2 mb-2 hover:text-white">{{ response.response }}</button>
+								<button class="border border-gray-200 bg-white hover:bg-indigo-500 rounded-lg py-1 px-2 ml-2 mb-2 transition hover:text-white">{{ response.response }}</button>
 							</div>
 						</div>
 
 						<div class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
 							<div>
-								<button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
+								<button class="transition flex items-center justify-center text-gray-400 hover:text-gray-600">
 									<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
 									</svg>
@@ -72,7 +72,7 @@
 							<div class="flex-grow ml-4">
 								<div class="relative w-full">
 									<input type="text" class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10 bg-gray-100" />
-									<button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+									<button class="transition absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
 										<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
 										</svg>
@@ -80,7 +80,7 @@
 								</div>
 							</div>
 							<div class="ml-4">
-								<button class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+								<button class="transition flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
 									<span>Send</span>
 									<span class="ml-2">
 										<svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -134,6 +134,12 @@ export default {
 </script>
 
 <style>
+* {
+	-webkit-user-select: none; /* Safari */
+	-ms-user-select: none; /* IE 10 and IE 11 */
+	user-select: none; /* Standard syntax */
+}
+
 /* width */
 ::-webkit-scrollbar {
 	width: 8px;
